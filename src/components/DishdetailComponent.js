d470a31a4e808b7e20f0cfe21b86a331f2cbf66e
TooLazy-QL <qiwenluo98@gmail.com>
2020-06-08 01:32:17 -0400

    spa 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,63 +1,73 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-    function RenderDish({dish}){
-        if (dish != null){
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
+function RenderDish({dish}) {
+    if(dish != null){
+        return (
+                <div className = 'col-12 col-md-5 m-1'>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+        )
     }
+}
 
-    function RenderComments({comments}) {
-		if(comments!= null){
-			return (
-				comments.map((comment, i) => {
-					return (
-						<ul className ='list-unstyled' key = {comment.id}>
-							<li>
-								<div>{comment.comment}</div>
-							    <div>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}`}</div>
-							</li>
-						</ul>
-					)
-				})		
+function RenderComments({comments}) {
+    if(comments!= null){
+        return (
+            <div className = 'col-12 col-md-5 m-1'>
+				{
+					comments.map((comment, i) => {
+						return (
+							<ul className ='list-unstyled' key = {comment.id}>
+								<li>
+									<div>{comment.comment}</div>
+								    <div>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}`}</div>
+								</li>
+							</ul>
+						)
+					})					
+				}
+			</div>
+
 			)
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
+		}
 	}
+
 
 
     const DishDetail = (props) => {
         console.log('Dishdetail Component render invoked')
         if(props.dish === null || props.dish === undefined) return (<div></div>)
 
+        const dish = props.dish
+		const comments = props.comments
+
         return (
             <div class="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-						<RenderComments comments={props.dish.comments} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
                     </div>
                 </div>
+                <div className = 'container'>
+			    	<div className = 'row'>
+						<RenderDish dish = {dish} />
+						<RenderComments comments = {comments} />			    	
+			    	</div>
+				</div>
             </div>
         );
     }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -19,7 +19,7 @@ function Home(props){
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} />
+                    <RenderCard item={props.dish} /> 
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.promotion} />
